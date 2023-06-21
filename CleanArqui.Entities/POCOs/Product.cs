3d58c4da240dd4bfbc5b05c930079608e526fac9
RenzoGu <201810589@urp.edu.pe>
2023-06-21 00:06:42 -0500

    Creation Data Context EFCore

--- CleanArqui.Entities/POCOs/Product.cs
+++ CleanArqui.Entities/POCOs/Product.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace CleanArqui.Entities.POCOs
 {
-    class Product
+    public class Product
     {
         public int Id { get; set; }
         public string Name { get; set; }
---
+++ CleanArqui.RepositoryEFCore/DataContext/CleanArquiContext.cs
@@ -0,0 +1,17 @@
+﻿using CleanArqui.Entities.POCOs;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArqui.RepositoryEFCore.DataContext
+{
+    public class CleanArquiContext : DbContext
+    {
+        public CleanArquiContext(DbContextOptions<CleanArquiContext> options) : base(options)  {  }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
