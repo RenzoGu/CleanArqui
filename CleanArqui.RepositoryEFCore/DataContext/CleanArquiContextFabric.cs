38b0fae6519e00e1d1ee3d70c1c814f87c1195c2
RenzoGu <201810589@urp.edu.pe>
2023-06-21 00:39:10 -0500

    Creation Context BD and First Migration

---
+++ CleanArqui.RepositoryEFCore/DataContext/CleanArquiContextFabric.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArqui.RepositoryEFCore.DataContext
+{
+    public class CleanArquiContextFabric : IDesignTimeDbContextFactory<CleanArquiContext>
+    {
+        public CleanArquiContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new 
+                DbContextOptionsBuilder<CleanArquiContext>();
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;database=CleanArqui");
+            return new CleanArquiContext(optionsBuilder.Options);
+        }
+    }
+}
